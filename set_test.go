d888834c05118ac3ad4abcb67ea0ecfa557afe83
MhmdRyhn <rayhan.cse12@gmail.com>
2023-08-16 18:02:27 +0600

    Add uni tests for set operations and check between 2 sets functions

--- set_test.go
+++ set_test.go
@@ -9,6 +9,7 @@ import (
 func TestLen(t *testing.T) {
 	si := New[int]()
 	assert.Equal(t, si.Len(), 0)
+
 	si = New(1, 2)
 	assert.Equal(t, si.Len(), 2)
 }
@@ -16,44 +17,132 @@ func TestLen(t *testing.T) {
 func TestContains(t *testing.T) {
 	si := New[int]()
 	assert.Equal(t, si.Len(), 0)
+
 	ss := New("abc", "def")
-	assert.Equal(t, si.Len(), 2)
-	for _, item := range ss.Members() {
-		assert.True(t, ss.Contains(item))
-	}
+	assert.Equal(t, ss.Len(), 2)
+	assert.True(t, ss.Contains("abc"))
+	assert.True(t, ss.Contains("def"))
 }
 
 func TestAdd(t *testing.T) {
 	si := New(1, 2)
 	assert.Equal(t, si.Len(), 2)
-	for _, item := range si.Members() {
-		assert.True(t, si.Contains(item))
-	}
+	assert.True(t, si.Contains(1))
+	assert.True(t, si.Contains(2))
+
 	si.Add(3)
-	assert.Equal(t, si.Members(), []int{1, 2, 3})
 	assert.Equal(t, si.Len(), 3)
-	for _, item := range si.Members() {
-		assert.True(t, si.Contains(item))
-	}
+	assert.True(t, si.Contains(1))
+	assert.True(t, si.Contains(2))
+	assert.True(t, si.Contains(3))
 }
+
 func TestRemove(t *testing.T) {
-	si := New(1, 2)
-	assert.Equal(t, si.Members(), []int{1, 2})
-	si.Remove(1)
-	assert.Equal(t, si.Members(), []int{2})
+	si := New(1, 2, 3)
+	si.Remove(2)
+	assert.Equal(t, si.Len(), 2)
+	assert.True(t, si.Contains(1))
+	assert.True(t, si.Contains(3))
 }
 
 func TestAddThenRemove(t *testing.T) {
 	si := New(1, 2)
-	assert.Equal(t, si.Members(), []int{1, 2})
+	assert.Equal(t, si.Len(), 2)
+	assert.True(t, si.Contains(1))
+	assert.True(t, si.Contains(2))
+
 	si.Add(3)
 	assert.Equal(t, si.Len(), 3)
-	for _, item := range si.Members() {
-		assert.True(t, si.Contains(item))
-	}
+	assert.True(t, si.Contains(1))
+	assert.True(t, si.Contains(2))
+	assert.True(t, si.Contains(3))
+
 	si.Remove(2)
 	assert.Equal(t, si.Len(), 2)
-	for _, item := range si.Members() {
-		assert.True(t, si.Contains(item))
-	}
+	assert.True(t, si.Contains(1))
+	assert.True(t, si.Contains(3))
+}
+
+func TestUnion(t *testing.T) {
+	ss1 := New("a", "b")
+	ss2 := New("b", "c")
+	result := ss1.Union(ss2)
+	assert.Equal(t, result.Len(), 3)
+	assert.True(t, result.Contains("a"))
+	assert.True(t, result.Contains("b"))
+	assert.True(t, result.Contains("c"))
+}
+
+func TestIntersection(t *testing.T) {
+	ss1 := New("a", "b", "c")
+	ss2 := New("b", "c", "d")
+	result := ss1.Intersection(ss2)
+	assert.Equal(t, result.Len(), 2)
+	assert.True(t, result.Contains("b"))
+	assert.True(t, result.Contains("c"))
+}
+
+func TestDifference(t *testing.T) {
+	ss1 := New("a", "b", "c")
+	ss2 := New("c", "d")
+	result := ss1.Difference(ss2)
+	assert.Equal(t, result.Len(), 2)
+	assert.True(t, result.Contains("a"))
+	assert.True(t, result.Contains("b"))
+}
+
+func TestIsEqual(t *testing.T) {
+	si1 := New(1, 2)
+	si2 := New(1)
+	assert.False(t, si1.IsEqual(si2))
+
+	si1 = New(1, 2)
+	si2 = New(1, 2)
+	assert.True(t, si1.IsEqual(si2))
+}
+
+func TestIsDisjoint(t *testing.T) {
+	si1 := New[int]()
+	si2 := New[int]()
+	assert.False(t, si1.IsDisjoint(si2))
+
+	si1 = New(1, 2)
+	si2 = New(2, 3)
+	assert.False(t, si1.IsDisjoint(si2))
+
+	si1 = New[int]()
+	si2 = New(1, 2, 3)
+	assert.True(t, si1.IsDisjoint(si2))
+
+	si1 = New(1, 2)
+	si2 = New(3, 4)
+	assert.True(t, si1.IsDisjoint(si2))
+}
+
+func TestIsSubsetOf(t *testing.T) {
+	ss1 := New("a", "b")
+	ss2 := New("a", "b", "c")
+	assert.True(t, ss1.IsSubsetOf(ss2, false))
+
+	ss1 = New("a", "b", "c")
+	ss2 = New("a", "b", "c")
+	assert.True(t, ss1.IsSubsetOf(ss2, false))
+
+	ss1 = New("a", "d")
+	ss2 = New("a", "b", "c")
+	assert.False(t, ss1.IsSubsetOf(ss2, false))
+}
+
+func TestIsProperSubsetOf(t *testing.T) {
+	ss1 := New("a", "b")
+	ss2 := New("a", "b", "c")
+	assert.True(t, ss1.IsSubsetOf(ss2, true))
+
+	ss1 = New("a", "b", "c")
+	ss2 = New("a", "b", "c")
+	assert.False(t, ss1.IsSubsetOf(ss2, true))
+
+	ss1 = New("a", "d")
+	ss2 = New("a", "b", "c")
+	assert.False(t, ss1.IsSubsetOf(ss2, true))
 }
